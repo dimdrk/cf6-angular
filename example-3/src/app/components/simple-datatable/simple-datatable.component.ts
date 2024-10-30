134a351d6189fb201c5f80d978ebf7baacfcf968
DimDrk <dimdrk@gmail.com>
2024-10-31 00:05:01 +0200

    sortData update

--- example-3/src/app/components/simple-datatable/simple-datatable.component.ts
+++ example-3/src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { EPerson, ManyPerson } from '../../shared/interfaces/person';
+import { sortBy } from 'lodash-es';
 
 @Component({
-  selector: 'app-simple-datatable',
-  standalone: true,
-  imports: [],
-  templateUrl: './simple-datatable.component.html',
-  styleUrl: './simple-datatable.component.css'
+    selector: 'app-simple-datatable',
+    standalone: true,
+    imports: [],
+    templateUrl: './simple-datatable.component.html',
+    styleUrl: './simple-datatable.component.css'
 })
 export class SimpleDatatableComponent {
-  manyPerson = ManyPerson;
+    manyPerson = ManyPerson;
+
+    sortOrder = {
+        givenName: 'none',
+        surName: 'none',
+        age: 'none',
+        email: 'none',
+        address: 'none'
+    }
+
+	sortData(sortKey: keyof EPerson): void {
+		if (this.sortOrder[sortKey] === 'asc') {
+			this.sortOrder[sortKey] = 'dsc';
+			this.manyPerson = sortBy(this.manyPerson, sortKey).reverse();
+		} else {
+		this.sortOrder[sortKey] = 'asc';
+		this.manyPerson = sortBy(this.manyPerson, sortKey);
+		}
+	}
 }
