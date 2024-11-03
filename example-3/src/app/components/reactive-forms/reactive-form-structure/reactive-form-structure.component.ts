854f343f18538c9aca8a89c71d3835c7a0563d0b
DimDrk <dimdrk@gmail.com>
2024-11-03 10:15:10 +0200

    reactive form

---
+++ example-3/src/app/components/reactive-forms/reactive-form-structure/reactive-form-structure.component.ts
@@ -0,0 +1,42 @@
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-reactive-form-structure',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  templateUrl: './reactive-form-structure.component.html',
+  styleUrl: './reactive-form-structure.component.css'
+})
+export class ReactiveFormStructureComponent {
+
+  userForm = new FormGroup({
+    givenName: new FormControl('', Validators.required),
+    surName: new FormControl('', Validators.required),
+    age: new FormControl(18, [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.min(18),
+      Validators.max(100)
+    ]),
+    email: new FormControl('', [ Validators.required, Validators.email ]),
+    address: new FormControl('', Validators.required)
+  })
+
+  onSubmit(value: any) {
+    console.log(value);
+    this.userForm.reset();
+  }
+
+}
--- example-3/src/app/components/reactive-forms/reactive-forms.component.ts
+++ example-3/src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormStructureComponent } from './reactive-form-structure/reactive-form-structure.component';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormStructureComponent, PersonTableComponent, SimpleDatatableComponent],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.css'
 })
