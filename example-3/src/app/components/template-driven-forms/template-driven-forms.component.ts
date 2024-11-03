b84804792ca58c4257c091a325d5f36c5970b053
DimDrk <dimdrk@gmail.com>
2024-11-03 11:30:48 +0200

    template driven forms

--- example-3/src/app/components/template-driven-forms/template-driven-forms.component.ts
+++ example-3/src/app/components/template-driven-forms/template-driven-forms.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import { TemplateFormStructureComponent } from './template-form-structure/template-form-structure.component';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent, SimpleDatatableComponent, TemplateFormStructureComponent],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.css'
 })
