87404434f811f222db0df517ad5a156b9061cf6c
DimDrk <dimdrk@gmail.com>
2024-11-04 13:48:28 +0200

    updates

--- example-3/src/app/components/template-driven-forms/template-form-structure/template-form-structure.component.ts
+++ example-3/src/app/components/template-driven-forms/template-form-structure/template-form-structure.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-template-form-structure',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './template-form-structure.component.html',
   styleUrl: './template-form-structure.component.css'
 })
-export class TemplateFormStructureComponent {
+export class TemplateFormStructureComponent implements AfterViewInit {
+  @ViewChild('userForm', {static: false}) form: NgForm | undefined;
 
+  ngAfterViewInit(): void {
+      setTimeout(() => {
+        if (this.form) {
+          this.form.setValue({
+            givenName: "John",
+            surName: "Doe",
+            age: 30,
+            email: "john@aueb.gr",
+            address: "road1"
+          })
+        }
+      }, 0)
+  }
+
+  onSubmit(value: any) {
+    console.log(value);
+    console.log(this.form)
+    console.log(this.form?.value)
+  }
 }
