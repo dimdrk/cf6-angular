b7e110ce039658382081a3bacec5cb47ecd7fc29
DimDrk <dimdrk@gmail.com>
2024-11-04 14:45:12 +0200

    http servises

---
+++ example-3/src/app/components/http-client-example/http-client-example.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { JokesService } from '../../shared/services/jokes.service';
+
+@Component({
+  selector: 'app-http-client-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './http-client-example.component.html',
+  styleUrl: './http-client-example.component.css'
+})
+export class HttpClientExampleComponent {
+  jokesService = inject(JokesService);
+  dadJoke: string = '';
+  chuckNorrisJoke: string = '';
+
+  // constructor(jokesService: JokesService){}
+
+  ngOnInit(){
+    this.jokesService.getDadJokes()
+      .subscribe((data) => {
+        console.log(data)
+      });
+
+      this.jokesService.getChuckNorrisJoke()
+      .subscribe((data) => {
+        console.log(data)
+      });
+  }
+}
