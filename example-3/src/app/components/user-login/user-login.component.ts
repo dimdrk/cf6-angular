d4abb35eb7efa9ddaa023ff51df241e923853934
DimDrk <dimdrk@gmail.com>
2024-11-10 18:21:19 +0200

    authGuard

--- example-3/src/app/components/user-login/user-login.component.ts
+++ example-3/src/app/components/user-login/user-login.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Credentials } from '../../shared/interfaces/mongo-backend';
 import { UserService } from '../../shared/services/user.service';
 import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { LoggedInUser } from '../../shared/interfaces/mongo-backend';
 
 @Component({
   selector: 'app-user-login',
@@ -27,6 +29,16 @@ export class UserLoginComponent {
             next: (response) => {
                 const access_token = response.access_token;
                 console.log(access_token);
+                localStorage.setItem("access_token", access_token);
+
+                const decodedTokenSubject = jwtDecode(access_token)
+                    .sub as unknown as LoggedInUser;
+                console.log(decodedTokenSubject);
+
+                this.userSevice.user.set({
+                    fullname: decodedTokenSubject.fullname,
+                    email: decodedTokenSubject.email
+                });
                 this.router.navigate(['restricted-content-example']);
             },
             error: (error) => {
